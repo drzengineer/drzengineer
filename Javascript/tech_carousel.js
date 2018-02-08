5001b7962219de27c7af253528f98ada2c6ec523
David Rodriguez <31993822+drzengineer@users.noreply.github.com>
2018-02-07 20:16:39 -0500

    added tech section carousel

---
+++ Javascript/tech_carousel.js
@@ -0,0 +1,23 @@
+var items = document.getElementsByClassName('carousel_item');
+
+var start = 0;
+var pos = [];
+
+for (var i = 0; i < items.length; i++){
+  items[i].style.left = start + 'px';
+  pos.push(start);
+  start+= 300;
+}
+
+setInterval(slider, 10);
+
+function slider() {
+
+  for (var i = 0; i < items.length; i++){
+    if (items[i].style.left == '-300px'){
+      pos[i] = 2700;
+    }
+    pos[i]--;
+    items[i].style.left = pos[i] + 'px';
+  }
+}
