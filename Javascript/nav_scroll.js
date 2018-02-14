33996423340266b84658f56ab559cf2ec3b138dd
David Rodriguez <31993822+drzengineer@users.noreply.github.com>
2018-02-13 22:04:30 -0500

    new nav layout
    nav scroll now enabled for every click event using switch

--- Javascript/nav_scroll.js
+++ Javascript/nav_scroll.js
@@ -1,15 +1,45 @@
-var aboutListener = document.getElementsByClassName('about_scroll')
+var navScroll = document.getElementsByClassName('nav_scroll')
 
-for(var i = 0; i < aboutListener.length; i++){
-  aboutListener[i].addEventListener('click', aboutScroll, false);
+for(var i = 0; i < navScroll.length; i++){
+  navScroll[i].addEventListener('click', scroll, false);
 }
 
-function aboutScroll(){
-  var element = document.getElementById('about');
+function scroll(e){
+
+  var element = '';
+
+  switch (e.target.id) {
+    case 'logo_nav':
+      element = document.getElementById('intro');
+      break;
+    case 'about_nav':
+      element = document.getElementById('about');
+      break;
+    case 'tech_nav':
+      element = document.getElementById('tech');
+      break;
+    case 'projects_nav':
+      element = document.getElementById('projects');
+      break;
+    case 'contact_nav':
+      element = document.getElementById('contact');
+      break;
+    case 'arrow':
+      element = document.getElementById('about');
+      break;
+    default:
+      alert('someone probably changed id names');
+  }
+
   element = element.getBoundingClientRect();
 
+  var navSize = document.getElementById('nav');
+  navSize = navSize.getBoundingClientRect();
+
+  var scroll = element.top - navSize.height;
+
   window.scrollBy({
-    top: element.top,
+    top: scroll,
     behavior: 'smooth'
   });
 }
