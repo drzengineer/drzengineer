c7694505d9db8c2c39193b849a279ee1e18786ab
David Rodriguez <31993822+drzengineer@users.noreply.github.com>
2018-04-25 18:21:48 -0400

    Circles repelled by mouse position.

--- Javascript/canvas.js
+++ Javascript/canvas.js
@@ -2,7 +2,7 @@ var canvas = document.querySelector('canvas');
 
 canvas.width = innerWidth;
 canvas.height = innerHeight;
-var bubbleScreenRatio = innerWidth / 30;
+var bubbleScreenRatio = innerWidth / 30 % 500;
 if (innerWidth < 576) bubbleScreenRatio = 0;
 
 var c = canvas.getContext('2d');
@@ -11,8 +11,8 @@ var c = canvas.getContext('2d');
 var maxRadius = 120;
 
 var mouse = {
-  x: undefined,
-  y: undefined
+  x: 0,
+  y: 0
 }
 
 window.addEventListener('mousemove', function (e) {
@@ -23,17 +23,31 @@ window.addEventListener('mousemove', function (e) {
 window.addEventListener('resize', function (){
   canvas.width = innerWidth;
   canvas.height = innerHeight;
-  bubbleScreenRatio = innerWidth / 30;
+  bubbleScreenRatio = innerWidth / 30 % 500;
   if (innerWidth < 576) bubbleScreenRatio = 0;
 
   init();
 })
 
+var posRelativeToMouse = {
+  x: undefined,
+  y: undefined
+}
+var distance = undefined;
+var forceDirection = {
+  x: undefined,
+  y: undefined
+}
+var maxDistance = 150;
+var force = undefined;
+
 function Circle(x, y, dx, dy, radius){
   this.x = x;
   this.y = y;
   this.dx = dx;
+  this.originalDx = dx;
   this.dy = dy;
+  this.originalDy = dy;
   this.radius = radius;
   this.minRadius = radius;
 
@@ -45,22 +59,36 @@ function Circle(x, y, dx, dy, radius){
   }
 
   this.update = function(){
+    posRelativeToMouse = {
+      x: this.x - mouse.x,
+      y: this.y - mouse.y
+    };
+
+    distance = Math.sqrt(
+      posRelativeToMouse.x * posRelativeToMouse.x +
+      posRelativeToMouse.y * posRelativeToMouse.y
+    );
+
+    forceDirection = {
+      x: posRelativeToMouse.x / distance,
+      y: posRelativeToMouse.y / distance,
+    };
+
+    force = (maxDistance - distance) / maxDistance;
+    if (force < 0) {
+      if ( Math.abs(this.dx) > Math.abs(this.originalDx)) this.dx *= 0.99;
+      if ( Math.abs(this.dy) > Math.abs(this.originalDy)) this.dy *= 0.99;
+    }
+    else {
+      this.dx += forceDirection.x * force;
+      this.dy += forceDirection.y * force;
+    }
+
     if (this.x + this.radius > innerWidth || this.x - this.radius < 0) this.dx = -this.dx;
     if (this.y + this.radius > innerHeight || this.y - this.radius < 0) this.dy = -this.dy;
 
     this.x += this.dx;
     this.y += this.dy;
-
-    //mouse movement
-    if (mouse.x - this.x < 100 && mouse.x - this.x > -100
-        && mouse.y - this.y < 100 && mouse.y - this.y > -100){
-      if (this.radius < maxRadius){
-        this.radius++;
-      }
-    }
-    else if (this.radius > this.minRadius){
-      this.radius--;
-    }
 
     this.draw();
   }
@@ -71,21 +99,17 @@ var circleArray = [];
 function init(){
   circleArray = [];
   for (var i = 0; i < bubbleScreenRatio; i++){
-    var radius = Math.random() * 38 + 2;
-    var x = Math.random() * (innerWidth - radius * 2) + radius;
-    var y = Math.random() * (innerHeight - radius * 2) + radius;
-    var dx = (Math.random() - 0.5) * 8;
-    var dy = (Math.random() - 0.5) * 8;
+    var radius = Math.floor(Math.random() * 38 + 2);
+    var x = Math.floor(Math.random() * (innerWidth - radius * 2) + radius);
+    var y = Math.floor(Math.random() * (innerHeight - radius * 2) + radius);
+    var dx = Math.floor((Math.random() - 0.5) * 7) + 1;
+    var dy = Math.floor((Math.random() - 0.5) * 7) + 1;
     circleArray.push(new Circle(x, y, dx, dy, radius));
   }
 }
 
-
-
-
 function animate(){
   requestAnimationFrame(animate);
-
   c.clearRect(0, 0, innerWidth, innerHeight);
 
   for (var i = 0; i < circleArray.length; i++){
