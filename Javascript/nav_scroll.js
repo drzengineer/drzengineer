d8e4b537f5a0097c15723c23a811c4b26c79934c
David Rodriguez <31993822+drzengineer@users.noreply.github.com>
2018-04-25 18:33:46 -0400

    adjusted nav toggle point

--- Javascript/nav_scroll.js
+++ Javascript/nav_scroll.js
@@ -15,15 +15,13 @@ $('a[href^="#"]').on('click', function(event) {
 var navToggle = false;
 
 window.onscroll = function () {
-  if (window.pageYOffset > 120 && navToggle == false) {
-    // do dat thang
+  if (window.pageYOffset > 70 && navToggle == false) {
     document.getElementById('nav').style.height = 70 + 'px';
     document.getElementById('nav').style.opacity = '1';
     document.getElementById('nav').style.backgroundColor = '#222228';
     navToggle = true;
   }
-  else if (window.pageYOffset <= 120 && navToggle == true){
-    // do dat dang that
+  else if (window.pageYOffset <= 70 && navToggle == true){
     document.getElementById('nav').style.height = 120 + 'px';
     document.getElementById('nav').style.opacity = '.4';
     document.getElementById('nav').style.backgroundColor = 'transparent';
