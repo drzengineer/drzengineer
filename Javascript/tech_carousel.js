29ea89ba07a21a52ca7c7a5ecdb54609e90c7655
David Rodriguez <31993822+drzengineer@users.noreply.github.com>
2018-05-12 23:35:15 -0400

    project section template

--- Javascript/tech_carousel.js
+++ Javascript/tech_carousel.js
@@ -6,7 +6,7 @@ var pos = [];
 for (var i = 0; i < items.length; i++){
   items[i].style.left = start + 'px';
   pos.push(start);
-  start+= 275;
+  start+= 300;
 }
 
 setInterval(slider, 10);
@@ -14,8 +14,8 @@ setInterval(slider, 10);
 function slider() {
 
   for (var i = 0; i < items.length; i++){
-    if (items[i].style.left == '-250px'){
-      pos[i] = 2500;
+    if (items[i].style.left == '-300px'){
+      pos[i] =  items.length * 300 - 300;
     }
     pos[i]--;
     items[i].style.left = pos[i] + 'px';
